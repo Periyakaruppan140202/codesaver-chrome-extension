3a87da32c94142b40140757ec5c1c05c01050aec
Periyakaruppan <periyakaruppannagappan2020@gmail.com>
2022-01-05 20:06:23 +0530

    Updated auto save once code submitted

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
-console.log("This Background extension is running...!");
+console.log('This Background extension is running...!');
 
-function btnClicked(tab) {
-  chrome.tabs.sendMessage(tab.id, "start");
-}
-chrome.browserAction.onClicked.addListener(btnClicked);
+// function btnClicked(tab) {
+//   chrome.tabs.sendMessage(tab.id, "start");
+// }
+// chrome.browserAction.onClicked.addListener(btnClicked);
 
 let gotMessage = (message, sender, sendRes) => {
   let { title, text, code } = message;
-  console.log("Successfully Downloading: ", title);
-  var blob = new Blob([text, code], {
-    type: "text/plain",
-  });
+  console.log('Successfully Downloading: ', title);
+  var blob = new Blob(
+    ['PROBLEM DESCRIPTION:\n\n', text, '\n\n\nYOUR CODE:\n\n', code],
+    {
+      type: 'text/plain',
+    }
+  );
   var url = URL.createObjectURL(blob);
   chrome.downloads.download({
     url: url,
--- content.js
+++ content.js
@@ -1,19 +1,29 @@
-console.log("This is Content.js");
-let gotMessage = (message, sender, sendRes) => {
-  if (message === "start") {
+console.log('This is Content.js');
+// let gotMessage = (message, sender, sendRes) => {
+var btn = document.querySelector('#j_id_7t');
+btn &&
+  btn.addEventListener('click', () => {
+    console.log('Clicked');
     const title = document
-      .querySelector("#leftpane > div > div")
-      .textContent.replace(/\s\s+/g, "\n\n")
-      .trim();
+      .querySelector('#leftpane > div > div')
+      .textContent.trim();
     const text = document
-      .querySelector("#leftpane > div")
-      .textContent.replace(/\s\s+/g, "\n\n");
+      .querySelector('#leftpane > div')
+      .textContent.replace(/\s\s+/g, '\n\n')
+      .trim();
     const code = document
-      .querySelector("#rightpane > div")
-      .textContent.replace(/\s\s+/g, "\n\n");
+      .querySelector('#editortxtCode > div.ace_scroller > div')
+      .textContent.replace(/\s\s+/g, '\n\n')
+      .trim();
     console.log(title, text, code);
-
-    chrome.runtime.sendMessage({ title, text, code });
-  }
-};
-chrome.runtime.onMessage.addListener(gotMessage);
+    setTimeout(() => {
+      if (
+        document.querySelector('#successmsg > div.ui.label.green.big')
+          .textContent == 'Great! Your code has passed.'
+      ) {
+        chrome.runtime.sendMessage({ title, text, code });
+      }
+    }, 10000);
+  });
+// };
+// chrome.runtime.onMessage.addListener(gotMessage);
